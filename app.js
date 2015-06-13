103b1833a5580a032b4a3b89f968deb681bfbc43
Jonathan Boulanger <boulajp@mcmaster.ca>
2015-06-13 16:36:39 -0400

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express');
+	fs = require('fs');
+	app = express();
+
+
+var server = app.listen(3000, function {
+	console.log('The server has started on port 3000')
+});
+
+
+function onPost() {
+	app.post('')
+}
+
+onPost();
