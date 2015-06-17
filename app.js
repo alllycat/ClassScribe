b13ae0e195efe69bf00ce85cfce22386119b6c09
Jonathan Boulanger <boulajp@mcmaster.ca>
2015-06-17 19:34:57 -0400

    removed redundant audio files

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // Global variables
 var express = require('express')
 	fs = require('fs')
-	app = express();
-	apikey = 'c12920b8-d1f7-4fb0-b96c-bbfb0df79d6a'
+	app = express()
 	request = require('request')
-
+	iod = require('iod-node')
+	client = new iod.IODClient('http://api.idolondemand.com','c12920b8-d1f7-4fb0-b96c-bbfb0df79d6a')
 
 // Setup
-app.set('views',__dirname + '.') // Use the views folder to hold html
+app.set('views',__dirname + '/frontend') // Use the views folder to hold html
 app.engine('html', require('ejs').renderFile); // Use ejs as a render engine
-app.use(express.static('.')) // Makes the static files available for get requests
+app.use(express.static('frontend')) // Makes the static files available for get requests
 
 // Listen for requests
 var server = app.listen(3000, function () {
@@ -26,28 +26,36 @@ var server = app.listen(3000, function () {
 
 function onPostRequest() {
 	// When there is a post request 
-	app.post('index.html', function (req, res) {
+	app.post('/upload.html', function (req, res) {
+		req.setEncoding('utf8')
 		console.log('Post request received')
 		req.on('data', function (data) {
+			console.log(data)
 			// Save data to referenceable audio file
-			var audiopath = __dirname + '/bin/audio'
+			var audiopath = __dirname + '/bin/audio1.mp3'
 			fs.writeFile(audiopath, data)
+			console.log('The audio file was created')
 			// Convert the audio data (audio) to text
-			convertToText(audiopath, res)
+			convertToText(__dirname + '/bin/Untitled.m4a', res)
 		})
 	})
 }
 function convertToText (audiopath, res) {
 	// Transcribe the audio into text
-	var commandurl = 'https://api.idolondemand.com/1/api/sync/recognizespeech/v1' + '?file=' + audiopath + '&apikey=' + apikey
-	request(commandurl, function (e, response, text) {
-		// Save data to referencable text file
-		var fulltextpath = __dirname + '/bin/fulltext.txt'
-		fs.writeFile(fulltextpath, text)
+	client.call('recognizespeech', 'data', function (e, response, data) {
+		console.log('Got job')
+		var jobID = data['data']['jobID']
+		// Get the result from jobID
+		console.log('https://api.idolondemand.com/1/job/result/' + jobID +'?apikey=' + 'c12920b8-d1f7-4fb0-b96c-bbfb0df79d6a')
+		request('https://api.idolondemand.com/1/job/result/' + jobID +'?apikey=' + 'c12920b8-d1f7-4fb0-b96c-bbfb0df79d6a', function (e1, response1, body1) {
+			console.log('Got text')
+			console.log(body1[0])
+			// 
+		})
 
-		res.send(text)
+		
 
-	})
+	}, {'file':'bin/Untitled.m4a'}, true)
 }
 
 onPostRequest()
